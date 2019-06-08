9e73760f20b61d03311f43f0fc5383b2ca79b6d8
Sunny Wong <Sunny@sunnywongsimac.lan>
2019-06-07 23:09:07 -0700

    academic

--- routes/academic.js
+++ routes/academic.js
@@ -34,8 +34,8 @@ var languageChecker = (req, res, next) => {
 
 router.get('/instructors', languageChecker, function (req, res, next) {
     let otherLocaleRef = req.i18n.getLocale() == 'en'
-      ? '/?lang=zh'
-      : '/?lang=en';
+      ? '/academic/instructors?lang=zh'
+      : '/academic/instructors?lang=en';
   
     res.render('academic/instructors', {
       otherLocaleRef: otherLocaleRef
@@ -44,8 +44,8 @@ router.get('/instructors', languageChecker, function (req, res, next) {
   
   router.get('/instructors2', languageChecker, function (req, res, next) {
     let otherLocaleRef = req.i18n.getLocale() == 'en'
-      ? '/?lang=zh'
-      : '/?lang=en';
+      ? '/academic/instructors2?lang=zh'
+      : '/academic/instructors2?lang=en';
   
     res.render('academic/instructors2', {
       otherLocaleRef: otherLocaleRef
