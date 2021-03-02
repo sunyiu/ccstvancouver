ec79c1fe7832cae5fa8e6f46bb0ca9542455e5f5
Sunny Wong <suunnn@hotmail.com>
2021-03-02 12:30:35 -0800

    new etc event

--- routes/events.js
+++ routes/events.js
@@ -3,22 +3,10 @@ var router = express.Router();
 
 router.get('/theology', function (req, res, next) {
   res.render('events/theology');
-});
+});;
 
-router.get('/2020_year_end', function (req, res, next) {
-  res.render('events/2020_year_end');
-});
-
-router.get('/mar22', function (req, res, next) {
-  res.render('events/mar22');
-});
-
-router.get('/blm', function (req, res, next) {
-  res.render('events/blm');
-});
-
-router.get('/hist31', function (req, res, next) {
-  res.render('events/hist31');
+router.get('/etc', function (req, res, next) {
+  res.render('events/etc');
 });
 
 
