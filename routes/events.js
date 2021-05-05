807a18c709e56754863acef62fbd336eac86556f
Sunny Wong <suunnn@hotmail.com>
2021-05-04 21:56:33 -0700

    may update part 1

--- routes/events.js
+++ routes/events.js
@@ -9,14 +9,6 @@ router.get('/dipcs', function (req, res, next) {
   res.render('events/dipcs');
 });
 
-router.get('/etc', function (req, res, next) {
-  res.render('events/etc');
-});
-
-router.get('/etc2', function (req, res, next) {
-  res.render('events/etc2');
-});
-
 router.get('/admin-info', function (req, res, next) {
   res.render('events/admin-info');
 });
